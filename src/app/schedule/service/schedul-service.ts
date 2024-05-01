4c8ea5901342b84f6f8b1e704bfb5418dbcd81db
geonwoo92 <cnu201102054@gmail.com>
2024-05-01 18:24:38 +0900

    240501 dev-gwl

--- src/app/schedule/service/schedul-service.ts
+++ src/app/schedule/service/schedul-service.ts
@@ -4,7 +4,7 @@ export const findStadiumNamesByDateRangeKey = ['getScheduleByDate']
 
 export const findStadiumNamesByDateRange = async () => {
     const { data } = await instance().get('/search',
-    {params:{q:'schedule',oq:''}});
+    {params:{c:'schedule',q:'22',oq:''}});
 
      return data
     }
