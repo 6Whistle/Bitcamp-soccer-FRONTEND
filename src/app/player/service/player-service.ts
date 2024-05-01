4bf8d5960995cc8bfb193d5ef3befe87156585ef
chan0326 <qkrdydcksz48@gmail.com>
2024-05-01 18:42:18 +0900

    2024-05-01

--- src/app/player/service/player-service.ts
+++ src/app/player/service/player-service.ts
@@ -2,79 +2,79 @@ import { instance } from "config/axios-config"
 
 export const getOnPositionKey = ['getOnPosition']
 export const getOnPosition = async () => {
-        const { data } = await instance().get(`/search`, { params : { "q": "2", "oq": "" } })
+        const { data } = await instance().get(`/search`, { params : {c: 'player',q: '2', oq: "" } })
     return data;
 }
 
 
 export const getOnPositionNotNullKey = ['getOnPositionNotNull']
 export const getOnPositionNotNull = async () =>{
-    const { data } = await instance().get('/search', {params : {q:'player', oq:''}});
+    const { data } = await instance().get('/search', {params : {c:'player',q:'3', oq:''}});
     return data
 }
 
 export const getOnPositionAndTeamIdKey = ['getOnPositionAndTeamId']
 export const getOnPositionAndTeamId = async () =>{
-    const { data } = await instance().get('/search', {params : {q:'player', oq:''}});
+    const { data } = await instance().get('/search', {params : {c:'player',q:'4', oq:''}});
     return data
 }
 
 export const getOnPositionAndTeamId2Key = ['getOnPositionAndTeamId2']
 export const getOnPositionAndTeamId2 = async () =>{
-    const { data } = await instance().get('/search', {params : {q:'player', oq:''}});
+    const { data } = await instance().get('/search', {params : {c:'player',q:'4-1', oq:''}});
     return data
 }
 
 export const getOnPositionAndTeamId7Key = ['getOnPositionAndTeamId7']
 export const getOnPositionAndTeamId7 = async () =>{
-    const { data } = await instance().get('/search', {params : {q:'player', oq:''}});
+    const { data } = await instance().get('/search', {params : {c:'player',q:'5', oq:''}});
     return data
 }
 
 export const getOnHeightAndWeightKey = ['getOnHeightAndWeight']
 export const getOnHeightAndWeight = async () =>{
-    const { data } = await instance().get('/search', {params : {q:'player', oq:''}});
+    const { data } = await instance().get('/search', {params : {c:'player',q:'5-1', oq:''}});
     return data
 }
 
 export const getOnPositionAndTeamId10Key = ['getOnPositionAndTeamId10']
 export const getOnPositionAndTeamId10 = async () =>{
-    const { data } = await instance().get('/search', {params : {q:'player', oq:''}});
+    const { data } = await instance().get('/search', {params : {c:'player',q:'6', oq:''}});
     return data
 }
 
 export const getOnCountAllKey = ['getOnCountAll']
 export const getOnCountAll = async () =>{
-    const { data } = await instance().get('/search', {params : {q:'player', oq:''}});
+    const { data } = await instance().get('/search', {params : {c:'player',q:'8', oq:''}});
     return data
 }
 
 export const getOnPositionAndTeamId20Key = ['getOnPositionAndTeamId20']
 export const getOnPositionAndTeamId20 = async () =>{
-    const { data } = await instance().get('/search', {params : {q:'player', oq:''}});
+    const { data } = await instance().get('/search', {params : {c:'player',q:'9', oq:''}});
     return data
 }
 
 export const getOnPositionAndTeamId21Key = ['getOnPositionAndTeamId21']
 export const getOnPositionAndTeamId21 = async () =>{
-    const { data } = await instance().get('/search', {params : {q:'player', oq:''}});
+    const { data } = await instance().get('/search', {params : {c:'player',q:'10', oq:''}});
     return data
 }
 
 export const getOnPositionAndTeamIdAndHeightKey = ['getOnPositionAndTeamIdAndHeight']
 export const getOnPositionAndTeamIdAndHeight = async () =>{
-    const { data } = await instance().get('/search', {params : {q:'player', oq:''}});
+    const { data } = await instance().get('/search', {params : {c:'player',q:'18', oq:''}});
     return data
 }
 
 export const getOnPositionAndTeamIdAndHeight2Key = ['getOnPositionAndTeamIdAndHeight2']
 export const getOnPositionAndTeamIdAndHeight2 = async () =>{
-    const { data } = await instance().get('/search', {params : {q:'player', oq:''}});
+    const { data } = await instance().get('/search', {params : {c:'player',q:'20', oq:''}});
     return data
 }
 
 export const getOnPositionAndHeightAndTeamIdKey = ['getOnPositionAndHeightAndTeamId']
 export const getOnPositionAndHeightAndTeamId = async () =>{
-    const { data } = await instance().get('/search', {params : {q:'player', oq:''}});
+    const { data } = await instance().get('/search', {params : {c:'player',q:'21', oq:''}});
     return data
 }
