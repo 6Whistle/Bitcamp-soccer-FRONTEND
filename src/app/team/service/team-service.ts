a9418a0d3d6c5ce9cacb8fc8c838f5da2bf5af32
6whistle <camjun0506@icloud.com>
2024-05-01 18:24:50 +0900

    ADD team/service 2024-05-01 jhl

--- src/app/team/service/team-service.ts
+++ src/app/team/service/team-service.ts
@@ -1,26 +1,15 @@
 import { instance } from "config/axios-config"
 
 export const getHeightAvgByTeamKey = ['getHeightAvgByTeam']
-export const getHeightAvgByTeam = async () => {
-        const { data } = await instance().get(`/search`, { params : { "q": "team", "oq": "height-avg" } })
-    return data;
-}
+export const getHeightAvgByTeam = async () => (await instance().get(`/search`, { params : { "c": "team", "q": "1", "oq": "" } })).data
+
 
 export const getTeamsKey = ['getTeams']
-export const getTeams = async () => {
-        const { data } = await instance().get(`/search`, { params : { "q": "team", "oq": "team-name-order" } })
-    return data;
-}
+export const getTeams = async () => (await instance().get(`/search`, { params : { "c": "team", "q": "12", "oq": "" } })).data
 
 export const getNotSelectedPostionKey = ['getNotSelectedPostion']
-export const getNotSelectedPostion = async () => {
-        const { data } = await instance().get(`/search`, { params : { "q": "team", "oq": "no-position" } })
-    return data;
-}
+export const getNotSelectedPostion = async () => (await instance().get(`/search`, { params : { "c": "team", "q": "13", "oq": "" } })).data
 
 export const getTeamsByteamNameKey = ['getTeamsByteamName']
-export const getTeamsByteamName = async () => {
-        const { data } = await instance().get(`/search`, { params : { "q": "team", "oq": "team-name" } })
-    return data;
-}
+export const getTeamsByteamName = async () => (await instance().get(`/search`, { params : { "c": "team", "q": "19", "oq": "" } })).data
 
