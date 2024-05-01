93e0e6a8fa0782ea089c784faf041aae32df2bb8
seunghochoi9 <jackpot97912@gmail.com>
2024-05-01 18:24:25 +0900

    240501 shc router

--- src/app/stadium/service/stadium-service.ts
+++ src/app/stadium/service/stadium-service.ts
@@ -3,30 +3,30 @@ import { instance } from "config/axios-config";
 
 export const stadiumNameWithTeamKey = ['stadiumNameWithTeam']
 export const stadiumNameWithTeam = async () => {
-    const { data } = await instance().get(`/search`, {params: {q:'stadium', oq:'nameWithTeam'}});
+    const { data } = await instance().get(`/search`, {params: {c: 'stadium', q:'11', oc: 'stadium', oq:''}});
     return data
 }
 
 export const stadiumAndTeamAndScheduleKey = ['stadiumAndTeamAndSchedule']
 export const stadiumAndTeamAndSchedule = async () => {
-    const { data } = await instance().get(`/search`, {params: {q:'stadium', oq:'teamAndSchedule'}});
+    const { data } = await instance().get(`/search`, {params: {c: 'stadium', q:'14', oc: 'stadium', oq:''}});
     return data
 }
 
 export const pohangSteelersGkKey = ['pohangSteelersGk']
 export const pohangSteelersGk = async () => {
-    const { data } = await instance().get(`/search`, {params: {q:'stadium', oq:'pohangSteelersGk'}});
+    const { data } = await instance().get(`/search`, {params: {c: 'stadium', q:'15', oc: 'stadium', oq:''}});
     return data
 }
 
 export const homeTeamWinKey = ['homeTeamWin']
 export const homeTeamWin = async () => {
-    const { data } = await instance().get(`/search`, {params: {q:'stadium', oq:'homeTeamWin'}});
+    const { data } = await instance().get(`/search`, {params: {c: 'stadium', q:'16', oc: 'stadium', oq:''}});
     return data
 }
 
 export const noHomeTeamKey = ['noHomeTeam']
 export const noHomeTeam = async () => {
-    const { data } = await instance().get(`/search`, {params: {q:'stadium', oq:'noHomeTeam'}});
+    const { data } = await instance().get(`/search`, {params: {c: 'stadium', q:'17', oc: 'stadium', oq:''}});
     return data
 }
